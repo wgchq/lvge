eaef0f676e4e48da448accabadad2d480cbfb1b1
JGG <zjgavin@sina.cn>
2017-04-15 19:01:26 +0800

    增加登录页面和主页

--- app/src/main/java/lvge/com/myapp/MainActivity.java
+++ app/src/main/java/lvge/com/myapp/MainActivity.java
@@ -2,6 +2,8 @@ package lvge.com.myapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login_submit =  (Button) findViewById(R.id.login_submit);
+
+        login_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_mian_page);
+            }
+        });
     }
 }
