f5f04d1153a85c27973436692b13fa1d62f720be
JGG <zjgavin@sina.cn>
2017-04-15 19:38:50 +0800

    增加HTTPService

--- app/src/main/java/lvge/com/myapp/MainActivity.java
+++ app/src/main/java/lvge/com/myapp/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import lvge.com.myapp.service.HttpService;
-
 public class MainActivity extends Activity {
 
     @Override
