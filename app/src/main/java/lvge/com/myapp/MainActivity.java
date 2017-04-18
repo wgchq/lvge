5abb14582b8390c6b5ddd00dacbf174309e17e32
JGG <zjgavin@sina.cn>
2017-04-18 21:40:04 +0800

    增加Main Page 页面

--- app/src/main/java/lvge/com/myapp/MainActivity.java
+++ app/src/main/java/lvge/com/myapp/MainActivity.java
@@ -1,6 +1,7 @@
 package lvge.com.myapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,11 +14,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Button login_submit =  (Button) findViewById(R.id.login_submit);
 
-
         login_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity_mian_page);
+              startActivity(new Intent(MainActivity.this,MainPageActivity.class));
             }
         });
     }
