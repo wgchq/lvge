4b6c7f13ce9995eb563942d2948cbcae05c373d3
JGG <zjgavin@sina.cn>
2017-05-27 01:42:01 +0800

    更改登陆页面布局，增加主页策划菜单

--- app/src/main/java/lvge/com/myapp/MainPageActivity.java
+++ app/src/main/java/lvge/com/myapp/MainPageActivity.java
@@ -1,14 +1,53 @@
 package lvge.com.myapp;
 
+import android.animation.FloatEvaluator;
 import android.app.Activity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import lvge.com.myapp.ui.SlideMenu;
+
+import static lvge.com.myapp.constant.Constant.CONSTACTS;
+import static lvge.com.myapp.constant.Constant.SETTINGS;
+
 
 public class MainPageActivity extends Activity {
 
+    private SlideMenu mMenu;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_page);
+        mMenu = (SlideMenu) findViewById(R.id.id_menu);
+
+    }
+
+    public void toggleMenu(View view)
+    {
+        mMenu.toggle();
     }
 }
---
+++ app/src/main/java/lvge/com/myapp/ui/SlideMenu.java
@@ -0,0 +1,174 @@
+package lvge.com.myapp.ui;
+
+/**
+ * Created by JGG on 2017/5/26.
+ */
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+
+import com.nineoldandroids.view.ViewHelper;
+
+import lvge.com.myapp.R;
+import lvge.com.myapp.util.ScreenUtils;
+
+
+public class SlideMenu extends HorizontalScrollView {
+    /**
+     * 灞忓箷瀹藉害
+     */
+    private int mScreenWidth;
+    /**
+     * dp
+     */
+    private int mMenuRightPadding;
+    /**
+     * 鑿滃崟鐨勫搴�
+     */
+    private int mMenuWidth;
+    private int mHalfMenuWidth;
+
+    private boolean isOpen;
+
+    private boolean once;
+
+    private ViewGroup mMenu;
+    private ViewGroup mContent;
+
+    public SlideMenu(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+
+    }
+
+    public SlideMenu(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mScreenWidth = ScreenUtils.getScreenWidth(context);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.SlidingMenu, defStyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.SlidingMenu_rightPadding:
+                    mMenuRightPadding = a.getDimensionPixelSize(attr,
+                            (int) TypedValue.applyDimension(
+                                    TypedValue.COMPLEX_UNIT_DIP, 50f,
+                                    getResources().getDisplayMetrics()));// 榛樿涓�10DP
+                    break;
+            }
+        }
+        a.recycle();
+    }
+
+    public SlideMenu(Context context) {
+        this(context, null, 0);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        /**
+         * 鏄剧ず鐨勮缃竴涓搴�
+         */
+        if (!once) {
+            LinearLayout wrapper = (LinearLayout) getChildAt(0);
+            mMenu = (ViewGroup) wrapper.getChildAt(0);
+            mContent = (ViewGroup) wrapper.getChildAt(1);
+
+            mMenuWidth = mScreenWidth - mMenuRightPadding;
+            mHalfMenuWidth = mMenuWidth / 2;
+            mMenu.getLayoutParams().width = mMenuWidth;
+            mContent.getLayoutParams().width = mScreenWidth;
+
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        if (changed) {
+            // 灏嗚彍鍗曢殣钘�
+            this.scrollTo(mMenuWidth, 0);
+            once = true;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        switch (action) {
+            // Up鏃讹紝杩涜鍒ゆ柇锛屽鏋滄樉绀哄尯鍩熷ぇ浜庤彍鍗曞搴︿竴鍗婂垯瀹屽叏鏄剧ず锛屽惁鍒欓殣钘�
+            case MotionEvent.ACTION_UP:
+                int scrollX = getScrollX();
+                if (scrollX > mHalfMenuWidth) {
+                    this.smoothScrollTo(mMenuWidth, 0);
+                    isOpen = false;
+                } else {
+                    this.smoothScrollTo(0, 0);
+                    isOpen = true;
+                }
+                return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    /**
+     * 鎵撳紑鑿滃崟
+     */
+    public void openMenu() {
+        if (isOpen)
+            return;
+        this.smoothScrollTo(0, 0);
+        isOpen = true;
+    }
+
+    /**
+     * 鍏抽棴鑿滃崟
+     */
+    public void closeMenu() {
+        if (isOpen) {
+            this.smoothScrollTo(mMenuWidth, 0);
+            isOpen = false;
+        }
+    }
+
+    /**
+     * 鍒囨崲鑿滃崟鐘舵��
+     */
+    public void toggle() {
+        if (isOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        float scale = l * 1.0f / mMenuWidth;
+        float leftScale = 1 - 0.3f * scale;
+        float rightScale = 0.8f + scale * 0.2f;
+
+        ViewHelper.setScaleX(mMenu, leftScale);
+        ViewHelper.setScaleY(mMenu, leftScale);
+        ViewHelper.setAlpha(mMenu, 0.6f + 0.4f * (1 - scale));
+        ViewHelper.setTranslationX(mMenu, mMenuWidth * scale * 0.7f);
+
+        ViewHelper.setPivotX(mContent, 0);
+        ViewHelper.setPivotY(mContent, mContent.getHeight() / 2);
+        ViewHelper.setScaleX(mContent, rightScale);
+        ViewHelper.setScaleY(mContent, rightScale);
+
+    }
+
+}
+
