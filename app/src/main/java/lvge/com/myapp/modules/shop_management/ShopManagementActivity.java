b6f74cbbea1ff13d3e029d75f2bba67eb569664c
JGG <zjgavin@sina.cn>
2017-06-16 22:56:58 +0800

    增加模块架构

---
+++ app/src/main/java/lvge/com/myapp/modules/shop_management/ShopManagementActivity.java
@@ -0,0 +1,15 @@
+package lvge.com.myapp.modules.shop_management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lvge.com.myapp.R;
+
+public class ShopManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_management);
+    }
+}
