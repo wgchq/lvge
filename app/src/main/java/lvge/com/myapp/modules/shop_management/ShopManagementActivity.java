1af2f72ae217bb2c3b01895ec5e3742d0521ccac
JGG <zjgavin@sina.cn>
2017-06-25 17:24:24 +0800

    删除没有用的引用

--- app/src/main/java/lvge/com/myapp/modules/shop_management/ShopManagementActivity.java
+++ app/src/main/java/lvge/com/myapp/modules/shop_management/ShopManagementActivity.java
@@ -4,12 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
+
 
 import lvge.com.myapp.R;
 
