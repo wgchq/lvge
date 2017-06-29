96ecf62d69b999c6f9376e5869b65b2869a2916c
mac <193933721@qq.com>
2017-06-29 22:44:10 +0800

    xsmiss

---
+++ app/src/main/java/lvge/com/myapp/modules/commodity_management/CommodityCommonProblem.java
@@ -0,0 +1,15 @@
+package lvge.com.myapp.modules.commodity_management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lvge.com.myapp.R;
+
+public class CommodityCommonProblem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commodity_common_problem);
+    }
+}
