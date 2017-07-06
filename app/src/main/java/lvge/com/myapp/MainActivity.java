e817fba94084c740324b491d348ff27a7a2cc68c
mac <193933721@qq.com>
2017-07-07 00:01:35 +0800

    xsmiss

--- app/src/main/java/lvge/com/myapp/MainActivity.java
+++ app/src/main/java/lvge/com/myapp/MainActivity.java
@@ -3,8 +3,19 @@ package lvge.com.myapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.telecom.Call;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+import com.zhy.http.okhttp.callback.Callback;
+
+import org.json.JSONObject;
+import java.util.HashMap;
+
 
 public class MainActivity extends Activity {
 
@@ -17,8 +28,32 @@ public class MainActivity extends Activity {
         login_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              startActivity(new Intent(MainActivity.this,MainPageActivity.class));
+             // startActivity(new Intent(MainActivity.this,MainPageActivity.class));
+
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("userName","admin");
+                params.put("pwd","1");
+                JSONObject jsonObject = new JSONObject(params);
+
+                OkHttpUtils.postString()
+                           .url("http://www.lvgew.com/obdcarmarket/sellerapp/login.do?userName=1&pwd=2")
+                           .content(new Gson().toJson(jsonObject)).build()
+                           .execute(new MyStringCallback());
             }
         });
     }
+
+    public class MyStringCallback extends StringCallback{
+
+        @Override
+        public void onError(okhttp3.Call call, Exception e, int i) {
+
+        }
+
+        public void onResponse(String response, int id){
+
+
+        }
+    }
+
 }
