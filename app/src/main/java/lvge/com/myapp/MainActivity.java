15afe79bcaad4e0207cc8ac7aa8972d0416b5dd8
mac <193933721@qq.com>
2017-07-07 22:55:47 +0800

    xsmiss

--- app/src/main/java/lvge/com/myapp/MainActivity.java
+++ app/src/main/java/lvge/com/myapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.telecom.Call;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,9 +13,15 @@ import com.google.gson.Gson;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 import com.zhy.http.okhttp.callback.Callback;
+import com.zhy.http.okhttp.request.PostFileRequest;
 
 import org.json.JSONObject;
 import java.util.HashMap;
+
+import okhttp3.MediaType;
+import okhttp3.Request;
+
+import static android.R.attr.id;
 
 
 public class MainActivity extends Activity {
@@ -30,30 +37,42 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
              // startActivity(new Intent(MainActivity.this,MainPageActivity.class));
 
-                HashMap<String, String> params = new HashMap<String, String>();
-                params.put("userName","admin");
-                params.put("pwd","1");
-                JSONObject jsonObject = new JSONObject(params);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            HashMap<String, String> params = new HashMap<String, String>();
+                            params.put("userName","admin");
+                            params.put("pwd","1");
+                           // JSONObject jsonObject = new JSONObject(params);
 
-                OkHttpUtils.postString()
-                           .url("http://www.lvgew.com/obdcarmarket/sellerapp/login.do?userName=1&pwd=2")
-                           .content(new Gson().toJson(jsonObject)).build()
-                           .execute(new MyStringCallback());
+                            OkHttpUtils.get()
+                                    .url("http://www.lvgew.com/obdcarmarket/sellerapp/login.do?userName=admin &pwd=1")
+                                    //.mediaType(MediaType.parse("application/json;charset=utf-8"))
+                                   // .content(new Gson().toJson(params))
+                                    .build()
+                                    .execute(new MyStringCallback());
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+
             }
         });
     }
+     public  class MyStringCallback extends StringCallback{
 
-    public class MyStringCallback extends StringCallback{
-
-        @Override
-        public void onError(okhttp3.Call call, Exception e, int i) {
-
-        }
-
-        public void onResponse(String response, int id){
+            public void onError(okhttp3.Call call, Exception e, int id) {
+             e.printStackTrace();
+             //mTv.setText("onError:" + e.getMessage());
+             }
 
 
-        }
-    }
+             public void onResponse(String response,int id){
+                 //mTv.setText("onResponse:" + decodeUnicodeToString(response));
+                 }
+     }
 
 }
