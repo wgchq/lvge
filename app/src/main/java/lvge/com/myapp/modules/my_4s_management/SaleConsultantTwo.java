411fa23c85fc17df7cbd8318137fe8b01821321e
mac <193933721@qq.com>
2017-07-09 22:36:01 +0800

    xsmiss

---
+++ app/src/main/java/lvge/com/myapp/modules/my_4s_management/SaleConsultantTwo.java
@@ -0,0 +1,15 @@
+package lvge.com.myapp.modules.my_4s_management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lvge.com.myapp.R;
+
+public class SaleConsultantTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_consultant_two);
+    }
+}
