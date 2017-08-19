1ffe01363280ea34b4ce512f856bcf34b931a2b8
JGG <zjgavin@sina.cn>
2017-08-19 14:12:13 +0800

    修改 toolbar

--- app/src/main/java/lvge/com/myapp/MainPageActivity.java
+++ app/src/main/java/lvge/com/myapp/MainPageActivity.java
@@ -133,13 +133,13 @@ public class MainPageActivity extends Activity {
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
 
-/*        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
-        }*/
+        }
 
         DefaultFragment();
         bottomNavigationView.setOnNavigationItemSelectedListener(
