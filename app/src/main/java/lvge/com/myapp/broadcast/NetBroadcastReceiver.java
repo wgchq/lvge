087c76b7b98410ed3c65c4a345702234a8c7b25a
JGG <zjgavin@sina.cn>
2017-08-20 14:20:16 +0800

    上传图片 截图

--- app/src/main/java/lvge/com/myapp/broadcast/NetBroadcastReceiver.java
+++ app/src/main/java/lvge/com/myapp/broadcast/NetBroadcastReceiver.java
@@ -1,12 +1,19 @@
 package lvge.com.myapp.broadcast;
 
 
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.LocalActivityManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.widget.Toast;
 
 import lvge.com.myapp.util.NetworkUtil;
+
+import static android.content.Context.ACTIVITY_SERVICE;
 
 
 /**
@@ -18,7 +25,14 @@ public class NetBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String status = NetworkUtil.getConnectivityStatusString(context);
-
+        /*ActivityManager am = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        ComponentName cn = am.getRunningTasks(1).get(0).topActivity;
+       *//* Class.forName(cn.getClassName().toString());*//*
+        new AlertDialog.Builder()
+                .setTitle("Warnning")
+                .setMessage(
+                        "You forget to write the message. Do you want to fill out it ??")
+                .create().show();*/
         Toast.makeText(context, status, Toast.LENGTH_LONG).show();
 
     }
