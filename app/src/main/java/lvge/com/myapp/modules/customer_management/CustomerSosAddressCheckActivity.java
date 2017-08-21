5eb6bad66a6b2303b671c5a12ca2d708faad8aac
JGG <zjgavin@sina.cn>
2017-08-22 00:52:09 +0800

    客户详情大地图

--- app/src/main/java/lvge/com/myapp/modules/customer_management/CustomerSosAddressCheckActivity.java
+++ app/src/main/java/lvge/com/myapp/modules/customer_management/CustomerSosAddressCheckActivity.java
@@ -1,7 +1,12 @@
 package lvge.com.myapp.modules.customer_management;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
@@ -18,6 +23,15 @@ public class CustomerSosAddressCheckActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_sos_address_check);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
        //显示地图
         mapView = (MapView) findViewById(R.id.map);
         //必须要写
