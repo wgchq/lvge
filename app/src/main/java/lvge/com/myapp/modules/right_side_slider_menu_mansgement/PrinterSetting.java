e1fafa6857a8f14440dd40044fe309b2e8fb85cf
mac <193933721@qq.com>
2017-10-06 15:56:46 +0800

    xs

--- app/src/main/java/lvge/com/myapp/modules/right_side_slider_menu_mansgement/PrinterSetting.java
+++ app/src/main/java/lvge/com/myapp/modules/right_side_slider_menu_mansgement/PrinterSetting.java
@@ -2,6 +2,8 @@ package lvge.com.myapp.modules.right_side_slider_menu_mansgement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import lvge.com.myapp.R;
 
@@ -11,5 +13,17 @@ public class PrinterSetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_printer_setting);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.pirinter_setting_toobar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(SalesConsultant.this, My4sManagementActivity.class);
+                // startActivity(intent);
+                finish();
+            }
+        });
     }
 }
